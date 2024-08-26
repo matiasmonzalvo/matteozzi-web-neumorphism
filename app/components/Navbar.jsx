af8c0677a946026980d9066307de0623e8b7a9b5
Monza <matiasmonzalvoarg@gmail.com>
2024-08-26 00:22:51 -0300

    Navbar Padding

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
 
   return (
     <nav className="bg-[#eeeeee] w-full shadow-lg fixed z-[40]">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-7">
+      <div className="w-full mx-auto px-[5%] lg:px-[5%]">
         <div className="flex justify-between h-16">
           {/* Logo */}
           <div className="flex-shrink-0 flex items-center">
@@ -91,17 +91,17 @@ const Navbar = () => {
 
       {/* Mobile menu, show/hide based on menu state. */}
       <div className={`${isMenuOpen ? 'block' : 'hidden'} lg:hidden`}>
-        <div className="px-2 pt-2 pb-3 space-y-1">
+        <div className="px-[5%] pt-2 pb-3 space-y-1">
           {menuItems.map((item) => (
             <a
               key={item.name}
               href={item.href}
-              className="text-gray-700 hover:bg-gray-100 hover:text-gray-900 block px-3 py-2 rounded-md text-base font-medium"
+              className="text-gray-700 hover:bg-gray-100 hover:text-gray-900 block py-2 rounded-md text-base font-medium"
             >
               {item.name}
             </a>
           ))}
-          <button className="w-full text-center bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mt-4">
+          <button className="w-full text-center bg-black hover:bg-blue-500 transition text-white font-bold py-2 px-4 rounded mt-4">
             Contáctanos
           </button>
         </div>
