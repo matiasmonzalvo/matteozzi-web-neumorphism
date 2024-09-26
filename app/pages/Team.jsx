039bace8f38a7a2f1220edb9a26a0af2b20c210e
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-09-26 18:59:48 -0300

    Team V2

--- app/pages/Team.jsx
+++ app/pages/Team.jsx
@@ -1,73 +1,104 @@
-import React, { useEffect } from 'react';
+import { FaLinkedin } from 'react-icons/fa';
 
 const Team = () => {
-  useEffect(() => {
-    const updateScrollSpeedCards = () => {
-      const viewportWidth = window.innerWidth;
-
-      const firstCard = document.querySelector('.team-card[data-scroll-speed="2"]');
-      const secondCard = document.querySelector('.team-card[data-scroll-speed="4"]');
-      const thirdCard = document.querySelector('.team-card[data-scroll-speed="6"]');
-
-      if (viewportWidth < 1024) {
-        if (firstCard) firstCard.setAttribute('data-scroll-speed', '0');
-        if (secondCard) secondCard.setAttribute('data-scroll-speed', '0');
-        if (thirdCard) thirdCard.setAttribute('data-scroll-speed', '0');
-      } else {
-        if (firstCard) firstCard.setAttribute('data-scroll-speed', '2');
-        if (secondCard) secondCard.setAttribute('data-scroll-speed', '4');
-        if (thirdCard) thirdCard.setAttribute('data-scroll-speed', '6');
-      }
-    };
-
-    updateScrollSpeedCards(); 
-    window.addEventListener('resize', updateScrollSpeedCards);
-
-    return () => {
-      window.removeEventListener('resize', updateScrollSpeedCards);
-    };
-  }, []);
-
   return (
-      <section className='w-screen h-auto pt-20 lg:pt-0 pb-40 lg:pb-10 lg:h-auto flex items-center justify-center' data-scroll-section>
-      <div className="flex flex-col w-full h-full lg:p-20 justify-center items-center lg:items-start">
-        <div className="w-auto"><h1 className="text-[10vw] leading-[1.2] lg:text-[5vw] font-[800]">Nuestro Equipo</h1></div>
-        <div className="flex flex-col items-center lg:items-start justify-center w-full h-[65%] lg:h-full relative gap-2 mt-10">
-          <div className="team-card w-[90%] lg:w-[55%] h-auto bg-white shadow-2xl rounded-3xl lg:ml-[5vw] flex flex-row px-4 lg:px-10 items-center py-5 lg:py-4" data-scroll data-scroll-speed="2" data-scroll-direction="horizontal">
-            <div className="w-[30%] lg:w-[17.5%] h-auto overflow-hidden rounded-full shadow-xl">
-              <img src="/marzorati.jpg" alt="" />
+    <section
+      className="bg-white text-black p-4 sm:px-8 sm:pt-32 pb-8 relative overflow-hidden"
+      data-scroll-section
+    >
+      <div className="max-w-7xl mx-auto relative z-10 py-6 sm:pt-16">
+        <div className="flex flex-col items-start justify-start w-full">
+          <span className="text-[2.5vw] sm:text-sm uppercase tracking-wider text-gray-600 mb-4 py-1 px-3 border border-gray-600 rounded-xl">
+            Team
+          </span>
+          <h1 className="text-3xl sm:text-[6.5vw] font-bold leading-tight sm:leading-[1]">
+            Nuestro Equipo
+          </h1>
+          <p className="text-sm sm:text-[0.9vw] text-gray-600 mt-4 sm:mt-6">
+            Conformado por profesionales altamente capacitados y comprometidos
+            con la innovación y el éxito de cada proyecto.
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center justify-center mt-16 text-center">
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 lg:w-[80%] text-center mt-16">
+            {/* CARD1 */}
+            <div className="h-[400px]">
+              <div className="relative flex flex-col h-[280px] rounded-2xl p-6 justify-between bg-gradient-to-tr from-emerald-100 to-emerald-300">
+                <a
+                  href="https://www.linkedin.com/in/username"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="absolute top-3 right-3 z-30 text-gray-50 hover:text-gray-100"
+                >
+                  <FaLinkedin className="w-8 h-8" />
+                </a>
+                <img
+                  src="/member1.png"
+                  alt="Member"
+                  className="absolute z-20 bottom-0 left-1/2 transform -translate-x-1/2 w-[400px] h-[400px] rounded-xl object-cover"
+                />
+              </div>
+              <h3 className="text-xl font-bold mt-4">
+                Tomás Agustín Matteozzi
+              </h3>
+              <span className="text-sm sm:text-[0.7vw] text-gray-600 mt-4 sm:mt-6 uppercase">
+                CEO & Fundador de la empresa
+              </span>
             </div>
-            <div className="w-[70%] lg:w-[70%] ml-[5vw] lg:ml-[2.5vw] text-black">
-              <span className='m-0 p-0 leading-[1]'>CEO</span>
-              <h3 className='text-[5.5vw] lg:text-[2.25vw] m-0 p-0 leading-[1] font-[700]'>Emiliano Matteozzi</h3>
-              <p className='lg:text-[1.25vw] font-[400] mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur libero aliquid non velit possimus fuga architecto necessitatibus, expedita eos quas!</p>
+            {/* CARD1 */}
+            <div className="h-[400px]">
+              <div className="relative flex flex-col h-[280px] rounded-2xl p-6 justify-between bg-gradient-to-tr from-sky-100 to-sky-300">
+                <a
+                  href="https://www.linkedin.com/in/username"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="absolute top-3 right-3 z-30 text-gray-50 hover:text-gray-100"
+                >
+                  <FaLinkedin className="w-8 h-8" />
+                </a>
+                <img
+                  src="/member1.png"
+                  alt="Member"
+                  className="absolute z-20 bottom-0 left-1/2 transform -translate-x-1/2 w-[400px] h-[400px] rounded-xl object-cover"
+                />
+              </div>
+              <h3 className="text-xl font-bold mt-4">
+                Tomás Agustín Matteozzi
+              </h3>
+              <span className="text-sm sm:text-[0.7vw] text-gray-600 mt-4 sm:mt-6 uppercase">
+                CEO & Fundador de la empresa
+              </span>
             </div>
-          </div>
-          <div className="team-card w-[90%] lg:w-[55%] h-auto bg-white shadow-2xl rounded-3xl lg:ml-[15vw] flex flex-row px-4 lg:px-10 items-center py-5 lg:py-4" data-scroll data-scroll-speed="4" data-scroll-direction="horizontal">
-            <div className="w-[30%] lg:w-[17.5%] h-auto overflow-hidden rounded-full shadow-xl">
-              <img src="/matteozzi.jpg" alt=""/>
-            </div>
-            <div className="w-[70%] lg:w-[70%] ml-[5vw] lg:ml-[2.5vw] text-black">
-              <span className='m-0 p-0 leading-[1]'>CEO</span>
-              <h3 className='text-[5.5vw] lg:text-[2.25vw] m-0 p-0 leading-[1] font-[700]'>Cesar Matteozzi</h3>
-              <p className='lg:text-[1.25vw] font-[400] mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur libero aliquid non velit possimus fuga architecto necessitatibus, expedita eos quas!</p>
-            </div>
-          </div>
-          <div className="team-card w-[90%] lg:w-[55%] h-auto bg-white shadow-2xl rounded-3xl lg:ml-[25vw] flex flex-row px-4 lg:px-10 items-center py-5 lg:py-4" data-scroll data-scroll-speed="6" data-scroll-direction="horizontal">
-            <div className="w-[30%] lg:w-[17.5%] h-auto overflow-hidden rounded-full shadow-xl">
-              <img src="/monzalvo.jpg" alt="" />
-            </div>
-            <div className="w-[70%] lg:w-[70%] ml-[5vw] lg:ml-[2.5vw] text-black">
-              <span className='m-0 p-0 leading-[1]'>CEO</span>
-              <h3 className='text-[5.5vw] lg:text-[2.25vw] m-0 p-0 leading-[1] font-[700]'>Stella Lavilla</h3>
-              <p className='lg:text-[1.25vw] font-[400] mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur libero aliquid non velit possimus fuga architecto necessitatibus, expedita eos quas!</p>
+            {/* CARD1 */}
+            <div className="h-[400px]">
+              <div className="relative flex flex-col h-[280px] rounded-2xl p-6 justify-between bg-gradient-to-tr from-cyan-100 to-cyan-300">
+                <a
+                  href="https://www.linkedin.com/in/username"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="absolute top-3 right-3 z-30 text-gray-50 hover:text-gray-100"
+                >
+                  <FaLinkedin className="w-8 h-8" />
+                </a>
+                <img
+                  src="/member1.png"
+                  alt="Member"
+                  className="absolute z-20 bottom-0 left-1/2 transform -translate-x-1/2 w-[400px] h-[400px] rounded-xl object-cover"
+                />
+              </div>
+              <h3 className="text-xl font-bold mt-4">
+                Tomás Agustín Matteozzi
+              </h3>
+              <span className="text-sm sm:text-[0.7vw] text-gray-600 mt-4 sm:mt-6 uppercase">
+                CEO & Fundador de la empresa
+              </span>
             </div>
           </div>
         </div>
       </div>
-      
     </section>
-  )
-}
+  );
+};
 
 export default Team;
