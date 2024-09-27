b97f222b136971028dd20ccfc44497af7b4c67c0
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-09-27 19:47:11 -0300

    Footer responsiveness

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
       className="w-full border-t border-gray-300 text-black py-12 lg:py-16"
       data-scroll-section
     >
-      <div className="container mx-auto px-4 lg:px-8 w-[80%]">
+      <div className="container mx-auto px-4 lg:px-8 w-[90%] lg:w-[80%]">
         <div className="flex flex-col lg:flex-row justify-between items-start lg:items-center">
           <div className="w-full lg:w-[40%] mb-8 lg:mb-0">
             <h2 className="text-3xl lg:text-4xl font-bold leading-tight mb-2">
@@ -13,28 +13,28 @@ export default function Footer() {
             </h2>
           </div>
           <div className="w-full lg:w-[35%]">
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 ">
+            <div className="flex flex-row gap-16">
               <div>
-                <h3 className="text-xl font-semibold mb-2">Links</h3>
+                <h3 className="text-md lg:text-xl font-bold mb-2">Links</h3>
                 <ul className="space-y-2">
                   <li className="flex items-center hover:underline cursor-pointer">
-                    <a className="text-xs">Nosotros</a>
+                    <a className="text-sm">Nosotros</a>
                   </li>
                   <li className="flex items-center hover:underline cursor-pointer">
-                    <a className="text-xs">Servicios</a>
+                    <a className="text-sm">Servicios</a>
                   </li>
                   <li className="flex items-center hover:underline cursor-pointer">
-                    <a className="text-xs">Contacto</a>
+                    <a className="text-sm">Contacto</a>
                   </li>
                 </ul>
               </div>
               <div>
-                <h3 className="text-xl font-semibold mb-2">Contacto</h3>
+                <h3 className="text-md lg:text-xl font-bold mb-2">Contacto</h3>
                 <ul className="space-y-2">
                   <li className="flex items-start">
                     <a
                       href="mailto:info@matteozzi-lavilla.com.ar"
-                      className="text-xs cursor-pointer hover:underline"
+                      className="text-sm cursor-pointer hover:underline"
                     >
                       info@matteozzi-lavilla.com.ar
                     </a>
@@ -45,9 +45,11 @@ export default function Footer() {
           </div>
         </div>
         <div className="mt-8 pt-4 border-t border-gray-800 flex flex-col lg:flex-row justify-between items-center">
-          <p className="text-xs text-gray-500">Developed by Mazzo</p>
-          <p className="text-xs text-gray-500 mt-2 lg:mt-0">
+          <p className="text-sm lg:text-xs text-gray-500 mt-2 lg:mt-0">
             © 2024 Matteozzi Lavilla. All Rights Reserved
+          </p>
+          <p className="text-sm lg:text-sm text-gray-500 mt-4 md:mt-0">
+            Developed by Mazzo
           </p>
         </div>
       </div>
