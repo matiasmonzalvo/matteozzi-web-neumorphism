0a0102a21051a957ff1d90c1e44e1ae59533a663
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-09-29 19:02:40 -0300

    False

--- app/page.js
+++ app/page.js
@@ -1,18 +1,18 @@
-'use client'
-import { useRef } from 'react'
-import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
-import 'locomotive-scroll/dist/locomotive-scroll.css'
-import './globals.css'
-import Footer from './components/Footer'
-import Hero from './pages/Hero'
-import About from './pages/About'
-import Service from './pages/Service'
-import Team from './pages/Team'
-import Contact from './pages/Contact'
-import Loader from './components/Loader'
+'use client';
+import { useRef } from 'react';
+import { LocomotiveScrollProvider } from 'react-locomotive-scroll';
+import 'locomotive-scroll/dist/locomotive-scroll.css';
+import './globals.css';
+import Footer from './components/Footer';
+import Hero from './pages/Hero';
+import About from './pages/About';
+import Service from './pages/Service';
+import Team from './pages/Team';
+import Contact from './pages/Contact';
+import Loader from './components/Loader';
 
 export default function Home() {
-  const containerRef = useRef(null)
+  const containerRef = useRef(null);
   // const [isLoading, setIsLoading] = useState(true)
 
   // useEffect(() => {
@@ -30,25 +30,25 @@ export default function Home() {
           smooth: true,
           smartphone: {
             breakpoint: 0,
-            smooth: true,
+            smooth: false
           },
           tablet: {
             breakpoint: 0,
-            smooth: true,
-          },
+            smooth: false
+          }
         }}
         watch={[]}
         containerRef={containerRef}
       >
         <div data-scroll-container ref={containerRef}>
-          <Hero/>
-          <About/>
-          <Service/>
-          <Team/>
-          <Contact/>
-          <Footer/>
+          <Hero />
+          <About />
+          <Service />
+          <Team />
+          <Contact />
+          <Footer />
         </div>
       </LocomotiveScrollProvider>
     </>
-  )
+  );
 }
